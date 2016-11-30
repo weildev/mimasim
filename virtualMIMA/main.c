776c2347a11cefa6eda20f24c8bbd15721d092b8
Jacques Jung <jacquesmarcojung@Jacquess-MacBook-Pro.local>
2016-11-30 15:08:31 +0100

    Initial Commit

---
+++ virtualMIMA/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  virtualMIMA
+//
+//  Created by Jacques Marco Jung on 30.11.16.
+//  Copyright © 2016 Jacques Marco Jung. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
